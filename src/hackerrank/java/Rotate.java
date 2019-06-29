e2a4b5df9452d6d0bb0330e4b0fe6021772c883d
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2019-06-29 11:07:34 -0300

    rotate was actually made in java

---
+++ src/hackerrank/java/Rotate.java
@@ -0,0 +1,24 @@
+public class Rotate {
+  public static int[] rotLeft(int[] a, int d) {
+    int len = a.length;
+    if (len == d)
+      return a;
+
+    Queue<Integer> q = new LinkedList<>();
+    for (int i = 0; i < d; i++)
+      q.add(a[i]);
+
+    int diff = len - d;
+    for (int i = d; i < len; i++) {
+      int newPos = i + diff;
+      if (newPos >= len)
+        newPos = newPos - len;
+
+      a[newPos] = a[i];
+    }
+    for (int i = (len - d); i < len; i++)
+      a[i] = q.poll();
+
+    return a;
+  }
+}
