dc6b8f69cd901694416352ee97b0c0fbd49c9520
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2019-07-13 15:33:19 -0300

    weighted strings with BigInteger

--- src/javas/src/strings/WeightedStrings.java
+++ src/javas/src/strings/WeightedStrings.java
@@ -1,42 +1,49 @@
 package strings;
 
+import java.math.BigInteger;
+
 public class WeightedStrings {
-
-  private Long[] internalWeights;
-  public Long[] weights(){
+  private BigInteger[] internalWeights;
+  private BigInteger[] weights(){
     if(internalWeights == null){
-      internalWeights = new Long[27];
-      internalWeights[0] = Long.MIN_VALUE;
+      internalWeights = new BigInteger[27];
+      internalWeights[0] = new BigInteger(Integer.toString(Integer.MIN_VALUE));
 
-      internalWeights[1] = Long.valueOf(1);
+      internalWeights[1] = BigInteger.valueOf(Long.valueOf(1));
       for(int i=2; i<= 26;i++)
-        internalWeights[i] =(i*internalWeights[i-1]) + internalWeights[i-1];
+        internalWeights[i] = BigInteger.valueOf(Long.valueOf(i))
+                                        .multiply(internalWeights[i-1])
+                                        .add(internalWeights[i-1]);
     }
 
     return internalWeights;
   }
 
-  public String calc(Long target) {
+  public WeightedStrings() {
+    weights();
+  }
+
+  public String calc(BigInteger target) {
     return solve(target, 1, ""); 
   }
 
-  private String solve(Long target, int lastPos, String res) {
-    if(target == 0) return res;
+  private String solve(BigInteger target, int lastPos, String res) {
+    if(target.compareTo(new BigInteger("0")) == 0) return res;
     
     if(weights()[lastPos] == target)
       return Character.valueOf((char)(64 + lastPos)) + res;
 
-    while(weights()[lastPos] <= target && lastPos < weights().length)
+    while(lastPos < (weights().length - 1) && weights()[lastPos].compareTo(target) <= 0)
       lastPos++;
 
-    while(weights()[lastPos] > target && lastPos > 0)
+    while(weights()[lastPos].compareTo(target) > 0 && lastPos > 0)
       lastPos--;
     
-    return solve(target - weights()[lastPos], lastPos, Character.valueOf((char)(64 + lastPos)) + res);
+    return solve(target.subtract(weights()[lastPos]), lastPos, Character.valueOf((char)(64 + lastPos)) + res);
   }
   public static void main(String[] args) {
     WeightedStrings ws = new WeightedStrings();
-    Long target = Long.valueOf(100);
+    BigInteger target = new BigInteger("2520");
     System.out.print("Target: " + target + " = " + ws.calc(target));
   }
 }
