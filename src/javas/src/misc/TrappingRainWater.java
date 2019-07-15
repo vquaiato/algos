fb1ebd5ed398305dfb2f4414588df6f894611cb6
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2019-07-14 23:30:38 -0300

    TrappingRainWater

---
+++ src/javas/src/misc/TrappingRainWater.java
@@ -0,0 +1,49 @@
+package misc;
+
+import java.util.Stack;
+
+public class TrappingRainWater {
+  public int trap(int[] h) {
+    if (h.length < 3) return 0;
+
+    Stack<Integer> s = new Stack<>();
+    int current = h[0];
+    int total = 0;
+
+    for (int i = 1; i < h.length; i++) {
+      if (h[i] < current) {
+        s.push(h[i]);
+      } else {
+        while (!s.isEmpty()) { total += (current - s.pop()); }
+        current = h[i];
+      }
+
+      if (i == h.length - 1 && !s.isEmpty()) {
+        int ch = s.pop();
+        if(ch < current)
+          current = ch;
+
+        while(!s.isEmpty()){
+          ch = s.pop();
+          if(current - ch > 0){ total += current - ch; }
+          else { current = ch; }
+        }
+      }
+    }
+
+    return total;
+  }
+
+  public static void main(String[] args) {
+    TrappingRainWater t = new TrappingRainWater();
+    //0, 1, 0, 2, 1, 0, 1, 1, 2, 1, 2, 1
+    //3,0,0,1
+    //3,0,1,0,3
+    //0,1,2,3,4,5
+    //5,4,3,2,1
+    //5,4,3,2,1,5
+
+    int r = t.trap(new int[] {5,4,3,2,1,5});
+    System.out.println(r);
+  }
+}
