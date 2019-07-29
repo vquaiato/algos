a781d239221d415a37bf147198a5277bc18b05bb
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2019-07-28 22:31:44 -0300

    Can attend meetings

---
+++ src/javas/src/misc/arrays/CanAttendMeetings.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/meeting-rooms
+
+package misc.arrays;
+
+import java.util.Arrays;
+
+public class CanAttendMeetings {
+  public boolean canAttendMeetings(int[][] intervals) {
+    if (intervals == null || intervals.length < 2)
+      return true;
+
+    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+    int ini = 0, end = 1;
+
+    for (int i = 1; i < intervals.length; i++)
+      if (intervals[i][ini] < intervals[i-1][end])
+        return false;
+
+    return true;
+  }
+}
