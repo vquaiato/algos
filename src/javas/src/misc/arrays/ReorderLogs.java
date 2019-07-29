f6f4314037be82a97c9636c962053faab7a0cd2e
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2019-07-28 22:33:35 -0300

    formatting :nail_care:

--- src/javas/src/misc/arrays/ReorderLogs.java
+++ src/javas/src/misc/arrays/ReorderLogs.java
@@ -7,54 +7,54 @@ import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-public class ReorderLogs{
-  private boolean isNumberLog(String log){
+public class ReorderLogs {
+  private boolean isNumberLog(String log) {
     int spc = log.indexOf(' ');
     return Character.isDigit(log.charAt(spc + 1));
   }
-  class Lex implements Comparator<String> 
-  { 
+
+  class Lex implements Comparator<String> {
     @Override
     public int compare(String s1, String s2) {
       int spc1 = s1.indexOf(' ');
       int spc2 = s2.indexOf(' ');
 
-      return s1.substring(spc1 + 1, s1.length())
-      .compareToIgnoreCase(s2.substring(spc2 + 1, s2.length())) == 0 ? 
-    s1.substring(0,spc1 - 1).compareToIgnoreCase(s2.substring(0, spc2-1)) : 
-    s1.substring(spc1 + 1, s1.length())
-      .compareToIgnoreCase(s2.substring(spc2 + 1, s2.length()));
+      return s1.substring(spc1 + 1, s1.length()).compareToIgnoreCase(s2.substring(spc2 + 1, s2.length())) == 0
+          ? s1.substring(0, spc1 - 1).compareToIgnoreCase(s2.substring(0, spc2 - 1))
+          : s1.substring(spc1 + 1, s1.length()).compareToIgnoreCase(s2.substring(spc2 + 1, s2.length()));
     }
-  } 
+  }
+
   public String[] reorderLogFiles(String[] logs) {
-    if(logs == null || logs.length < 2) return logs;
+    if (logs == null || logs.length < 2)
+      return logs;
 
     Queue<String> numberLogs = new LinkedList<>();
     PriorityQueue<String> letterLogs = new PriorityQueue<>(new Lex());
 
-    for(String log : logs){
-      if(isNumberLog(log))
+    for (String log : logs) {
+      if (isNumberLog(log))
         numberLogs.add(log);
       else
         letterLogs.add(log);
     }
 
     String[] res = new String[numberLogs.size() + letterLogs.size()];
-    int i=0;
-    while(!letterLogs.isEmpty())
+    int i = 0;
+    while (!letterLogs.isEmpty())
       res[i++] = letterLogs.poll();
-    
-    while(!numberLogs.isEmpty())
+
+    while (!numberLogs.isEmpty())
       res[i++] = numberLogs.poll();
 
     return res;
   }
 
   public static void main(String[] args) {
-    String[] input = {"a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"};
+    String[] input = { "a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo" };
     var res = new ReorderLogs().reorderLogFiles(input);
 
-    for(var l: res)
+    for (var l : res)
       System.out.println(l);
   }
 }
