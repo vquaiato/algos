b34057c047dd72088c76aca7d7b76bd83d3afc5a
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2019-08-02 21:59:37 -0300

    formatting

--- src/javas/src/strings/SuperReduced.java
+++ src/javas/src/strings/SuperReduced.java
@@ -1,36 +1,36 @@
 package strings;
 
-public class SuperReduced{
-  public static String superReducedString(String original, String actual){
+public class SuperReduced {
+  public static String superReducedString(String original, String actual) {
     StringBuffer res = new StringBuffer();
-    int i=0;
+    int i = 0;
     char[] input = (actual == null ? original : actual).toCharArray();
 
-    while(i<input.length){
-        if(i==(input.length-1) || input[i] != input[i+1]){
-            res.append(input[i]);
-            i++;
-        }
-        else
-            i+=2;
+    while (i < input.length) {
+      if (i == (input.length - 1) || input[i] != input[i + 1]) {
+        res.append(input[i]);
+        i++;
+      } else
+        i += 2;
     }
 
     String result = "";
-    if(res.length() == 0)
-        return "Empty String";
+    if (res.length() == 0)
+      return "Empty String";
     else
-        result = res.toString();
+      result = res.toString();
 
-    if(actual !=null && result.equals(actual))
-        return actual;
+    if (actual != null && result.equals(actual))
+      return actual;
     else
-        return superReducedString(original, result);
-}
-  public static String superReducedString(String s) {
-      return superReducedString(s, null);
+      return superReducedString(original, result);
   }
 
-  public static void main(String[] args){
+  public static String superReducedString(String s) {
+    return superReducedString(s, null);
+  }
+
+  public static void main(String[] args) {
     String r = SuperReduced.superReducedString("aaabccddd");
     System.out.println(r);
   }
