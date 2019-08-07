4ac1ed3f17e05f2e2070f2ea7599207f2758aebc
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2019-08-07 01:04:08 -0300

    Tree  node

---
+++ src/javas/src/trees/TreeNode.java
@@ -0,0 +1,14 @@
+package trees;
+
+public class TreeNode {
+  TreeNode left;
+  TreeNode right;
+  int value;
+
+  public TreeNode(int value) {
+    this.value = value;
+    this.left = this.right = null;
+  }
+
+  boolean isLeaf() {return this.left == null && this.right == null;}
+}
