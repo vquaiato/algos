fca195232d2fd4c490ece2273c7ba30519ef8933
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2019-08-11 23:10:45 -0300

    Parsing to Integer

--- src/javas/src/arrays/EvalReversePolishNotation.java
+++ src/javas/src/arrays/EvalReversePolishNotation.java
@@ -3,7 +3,6 @@
 package arrays;
 
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Deque;
 
 public class EvalReversePolishNotation {
@@ -11,17 +10,16 @@ public class EvalReversePolishNotation {
     if (tokens == null || tokens.length == 0)
       return 0;
 
-    Deque<String> stack = new ArrayDeque<>();
+    Deque<Integer> stack = new ArrayDeque<>();
+
     for (String token : tokens) {
       if (!"+-*/".contains(token))
-        stack.addFirst(token);
-      else {
-        Integer partial = calc(token, Integer.parseInt(stack.pop()), Integer.parseInt(stack.pop()));
-        stack.addFirst(Integer.toString(partial));
-      }
+        stack.addFirst(Integer.parseInt(token));
+      else
+        stack.addFirst(calc(token, stack.pop(), stack.pop()));
     }
 
-    return Integer.parseInt(stack.pop());
+    return stack.pop();
   }
 
   private int calc(String op, int operand1, int operand2) {
@@ -41,8 +39,7 @@ public class EvalReversePolishNotation {
     // String[] tokens = {"2"};
     // String[] tokens = {"2", "1", "+", "3", "*"};
     // String[] tokens = {"4", "13", "5", "/", "+"};
-    // String[] tokens = {"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+",
-    // "5", "+"};
+    String[] tokens = { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" };
 
     System.out.println(new EvalReversePolishNotation().evalRPN(tokens));
   }
