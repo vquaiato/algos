a693c73d6d1fa825728fd14438286fe316fe2972
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2019-08-12 19:52:20 -0300

    RPN

--- src/javas/src/arrays/EvalReversePolishNotation.java
+++ src/javas/src/arrays/EvalReversePolishNotation.java
@@ -36,10 +36,10 @@ public class EvalReversePolishNotation {
   }
 
   public static void main(String[] args) {
-    // String[] tokens = {"2"};
+    String[] tokens = {"2"};
     // String[] tokens = {"2", "1", "+", "3", "*"};
     // String[] tokens = {"4", "13", "5", "/", "+"};
-    String[] tokens = { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" };
+    // String[] tokens = { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" };
 
     System.out.println(new EvalReversePolishNotation().evalRPN(tokens));
   }
